a9777e9823ac7730edd2c6b485692c72bec4006a
lenhung1107 <tink60chuyenbienhoa@gmail.com>
2025-06-06 15:16:07 +0700

    add cors link product

--- server/index.js
+++ server/index.js
@@ -13,7 +13,8 @@ const cors = require('cors');
 const allowedOrigins = [
     'http://localhost:5173',
      'http://localhost:5174',
-    'https://gr2-o3rfs9qy4-nhungles-projects.vercel.app'
+    'https://gr2-o3rfs9qy4-nhungles-projects.vercel.app',
+    'https://gr2-liard.vercel.app/'
 ];
 
 app.use(cors({
