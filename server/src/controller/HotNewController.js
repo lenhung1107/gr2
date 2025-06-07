cfc48189ac252a70746442b3aa9e742e42fe6a42
lenhung1107 <tink60chuyenbienhoa@gmail.com>
2025-06-07 18:21:46 +0700

    add cosole api key

--- server/src/controller/HotNewController.js
+++ server/src/controller/HotNewController.js
@@ -1,11 +1,12 @@
 const Doctor = require("../models/Doctor");
-require("dotenv").config();
+require('dotenv').config();
 const axios = require("axios");
 class HotNewController {
   async getNew(req, res) {
     try {
+        console.log(process.env.NEWS_API_KEY);
       const response = await axios.get(
-        `https://newsapi.org/v2/everything?q=sức%20khoẻ&language=vi&pageSize=5&apiKey=${process.env.NEWS_API_KEY}`
+        `https://newsapi.org/v2/everything?q=s%E1%BB%A9c%20kho%E1%BA%BB&language=vi&pageSize=5&apiKey=${process.env.NEWS_API_KEY}`
       );
       res.json(response.data);
     } catch (error) {
