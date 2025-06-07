c33f9d566e7babd71f1e254fd49bf35b659444e7
lenhung1107 <tink60chuyenbienhoa@gmail.com>
2025-06-07 12:41:56 +0700

    Enable PWA and update configs

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,7 +4,9 @@ import { Provider } from "react-redux"
 import { store, persistor } from './redux/store.js'
 import App from './App.jsx'
 import GlobalStyles from './component/GlobalStyles/index.js';
-import { PersistGate } from 'redux-persist/integration/react'
+import { PersistGate } from 'redux-persist/integration/react';
+import { registerSW } from 'virtual:pwa-register';
+registerSW(); // Đăng ký Service Worker
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <GlobalStyles>
--- client/vite.config.js
+++ client/vite.config.js
@@ -1,8 +1,40 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/', // Đảm bảo đường dẫn gốc là "/"
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: [
+        "favicon.svg",
+        "favicon.ico",
+        "robots.txt",
+        "apple-touch-icon.png",
+      ],
+      manifest: {
+        name: "Booking Healthcare",
+        short_name: "Healthcare",
+        description: "Ứng dụng đặt lịch khám bệnh",
+        theme_color: "#1976d2",
+        background_color: "#ffffff",
+        display: "standalone",
+        start_url: "/",
+        icons: [
+          {
+            src: "/pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
+  base: "/", // Đảm bảo đường dẫn gốc là "/"
+});
