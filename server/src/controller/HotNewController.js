bd82366feea23ca542e3f98c2e81e766d6f7b820
lenhung1107 <tink60chuyenbienhoa@gmail.com>
2025-06-28 01:18:55 +0700

    fix hotnew link last

--- server/src/controller/HotNewController.js
+++ server/src/controller/HotNewController.js
@@ -6,10 +6,10 @@ class HotNewController {
     try {
       const response = await axios.get(`https://newsapi.org/v2/everything`, {
         params: {
-          q: "health OR medical",
+          q: "health OR medical OR medicine OR covid OR vaccine",
           sortBy: "publishedAt",
           pageSize: 5,
-          from: new Date(Date.now() - 3 * 24 * 60 * 60 * 1000).toISOString(),
+          from: new Date(Date.now() - 7 * 24 * 60 * 60 * 1000).toISOString(),
           apiKey: process.env.NEWS_API_KEY,
         },
       });
