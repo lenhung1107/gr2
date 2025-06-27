1ed3f6db84b7c7e52cc922a64d20d37eb15e4e2b
lenhung1107 <tink60chuyenbienhoa@gmail.com>
2025-06-28 01:13:34 +0700

    fix hot new link

--- server/src/controller/HotNewController.js
+++ server/src/controller/HotNewController.js
@@ -4,14 +4,19 @@ const axios = require("axios");
 class HotNewController {
   async getNew(req, res) {
     try {
-      const feed = await parser.parseURL(
-        "https://vnexpress.net/rss/suc-khoe.rss"
-      );
-      const articles = feed.items.slice(0, 5); 
-      res.json(articles);
+      const response = await axios.get(`https://newsapi.org/v2/everything`, {
+        params: {
+          q: "health OR medical",
+          sortBy: "publishedAt",
+          pageSize: 5,
+          from: new Date(Date.now() - 3 * 24 * 60 * 60 * 1000).toISOString(),
+          apiKey: process.env.NEWS_API_KEY,
+        },
+      });
+      res.json(response.data);
     } catch (error) {
       console.error(error);
-      res.status(500).json({ error: "Không lấy được tin tức từ RSS" });
+      res.status(500).json({ error: "Không lấy được tin tức" });
     }
   }
 }
